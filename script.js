37da96067f30fa07a19ea8c04ad76d5d9d04bea8
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-05 09:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
+
+function getFormvalue(e) {
     //Write your code here
+	e.preventDefault();
+	const form = document.getElementById('form1');
+	const first = form.elements['fname'].value.trim();
+	const last = form.elements['lname'].value.trim();
+	alert(`${first} ${last}`);
 
 }
